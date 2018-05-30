8b54f45fb452851e6b281e7568fdefe48ee1180e
Christopher Karras <ckarras11@gmail.com>
2018-05-29 20:45:05 -0400

    Removed unused fields from schema

--- models/user.js
+++ models/user.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
     name: { type: String, required: true },
-    companyCode: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
 });
